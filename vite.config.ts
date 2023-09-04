c568e801655b4ea5ba39f22559136b25159fb09c
Domenico Colandrea <Domenico.Colandrea@msci.com>
2023-09-04 10:34:32 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { defineConfig, loadEnv, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config https://vitest.dev/config
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/**.test.{ts,tsx}'],
-  },
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), [`VITE`, `REACT_APP`, `NODE_ENV`]);
+  const config = {
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: 'happy-dom',
+      setupFiles: '.vitest/setup',
+      include: ['**/**.test.{ts,tsx}'],
+    },
+    resolve: {
+      alias: [{ find: `@`, replacement: resolve(__dirname, `src`) }],
+    },
+    define: {
+      [`process.env`]: env,
+    },
+    server: {
+      proxy: {
+        [`/api`]: env.VITE_PROXY_URL,
+      },
+    },
+  };
+  return config;
 });
